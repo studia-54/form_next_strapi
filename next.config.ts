29be689b57872c8fda87f2ad6d48d6941ab6900e
Romil <khandryga.romil@gmail.com>
2025-02-03 17:55:58 +0300

    fix(cors): allow our sites

--- next.config.ts
+++ next.config.ts
@@ -1,27 +1,6 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: '*',
-          },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET, POST, OPTIONS',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value: 'X-Requested-With, Content-Type',
-          },
-        ],
-      },
-    ]
-  },
   images: {
     remotePatterns: [
       {
