dd7d836752fa5bb17718d63af2698c2951533bd2
Romil <khandryga.romil@gmail.com>
2025-02-03 17:46:11 +0300

    feat(cors): allow all

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,35 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, OPTIONS',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'X-Requested-With, Content-Type',
+          },
+        ],
+      },
+    ]
+  },
   images: {
     remotePatterns: [
-     {
-     protocol: "https",
-     hostname: "admin.forms.studia-54.com"
-     },
+      {
+        protocol: 'https',
+        hostname: 'admin.forms.studia-54.com',
+      },
     ],
-},
-};
+  },
+}
 
-export default nextConfig;
+export default nextConfig
