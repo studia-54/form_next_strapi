c73154a3f38de0f36402b9fdc295b09fca45fc23
annakrm <karmyzova.dev@gmail.com>
2025-02-19 12:23:56 +0300

    fix: hash

--- components/confident-checkbox/ConfidentCheckbox.tsx
+++ components/confident-checkbox/ConfidentCheckbox.tsx
@@ -1,14 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import checkedIcon from "@/shared/input_checked.svg";
-import { usePathname } from 'next/navigation';
 interface ConfidentCheckboxProps {
     isChecked: boolean;
     onCheckboxChange: () => void;
 }
 
 const ConfidentCheckbox: React.FC<ConfidentCheckboxProps> =  ({ isChecked, onCheckboxChange }) => {
-    const pathname = usePathname();
 
   return (
     <div className={styles.confident_container}>
@@ -36,7 +34,7 @@ const ConfidentCheckbox: React.FC<ConfidentCheckboxProps> =  ({ isChecked, onChe
         <div className={styles.text_box}>
             <span className={styles.confident_text}>Принимаю условия </span>
             <a
-            href={pathname.includes('studia-54') ? 'https://admin.forms.studia-54.com/uploads/Politika_Studia54_b0f85802a1.pdf' : 'https://admin.forms.studia-54.com/uploads/Politika_Fifty_767335c813.pdf'}
+            href={window.location.hash === '#fiftyfourms' ? 'https://admin.forms.studia-54.com/uploads/Politika_Fifty_767335c813.pdf' : 'https://admin.forms.studia-54.com/uploads/Politika_Studia54_b0f85802a1.pdf'}
                 target="_blank"
                 // download="confident_example.pdf"
                 >
