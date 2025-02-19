2c0bc755e354146453346508599ef91dbfa69b79
annakrm <karmyzova.dev@gmail.com>
2025-02-19 11:53:53 +0300

    feat: add privacy policy

--- components/confident-checkbox/ConfidentCheckbox.tsx
+++ components/confident-checkbox/ConfidentCheckbox.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import checkedIcon from "@/shared/input_checked.svg";
-
+import { usePathname } from 'next/navigation';
 interface ConfidentCheckboxProps {
     isChecked: boolean;
     onCheckboxChange: () => void;
 }
 
 const ConfidentCheckbox: React.FC<ConfidentCheckboxProps> =  ({ isChecked, onCheckboxChange }) => {
+    const pathname = usePathname();
 
   return (
     <div className={styles.confident_container}>
@@ -34,8 +35,8 @@ const ConfidentCheckbox: React.FC<ConfidentCheckboxProps> =  ({ isChecked, onChe
 
         <div className={styles.text_box}>
             <span className={styles.confident_text}>Принимаю условия </span>
-            {/* fix it */}
-            <a href="https://devstudia54comadminstrapi.studia-54.group/uploads/privacy_policy_da8b2257df.pdf"
+            <a
+            href={pathname.includes('studia-54') ? 'https://admin.forms.studia-54.com/uploads/Politika_Studia54_b0f85802a1.pdf' : 'https://admin.forms.studia-54.com/uploads/Politika_Fifty_767335c813.pdf'}
                 target="_blank"
                 // download="confident_example.pdf"
                 >
