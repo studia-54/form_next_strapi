26b249f9ca73042904ea4929adb9148d32cf4e2b
Romil <khandryga.romil@gmail.com>
2025-02-20 16:47:55 +0300

    feat: add Yandex Metrika tracking component and integrate it into the layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,26 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from 'next'
+import './globals.css'
+import { Suspense } from 'react'
+import { Metrika } from '@/components/Metrika/Metrika'
 
 export const metadata: Metadata = {
-  title: "Form for Studia 54",
-  description: "Form for Studia 54",
-};
+  title: 'Form for Studia 54',
+  description: 'Form for Studia 54',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-       <html lang="en">
-        <body>
-          {children}
-        </body>
-      </html>
-  );
+    <html lang="en">
+      <body>
+        <Suspense>
+          <Metrika />
+        </Suspense>
+        {children}
+      </body>
+    </html>
+  )
 }
